acf0d915c26dbbffa272b57f1ec15f1972f515fe
Gabriel Rabelo Camargos <contato.ideialisa@gmail.com>
2020-08-22 11:49:03 -0300

    add messages

--- src/validations/order.js
+++ src/validations/order.js
@@ -1,3 +1,6 @@
 import { check } from 'express-validator';
+import { validation as msg } from '../utils/messages';
 
-module.exports = { store: [check('email').isEmail()] };
+module.exports = {
+  store: [check('email').isEmail().withMessage(msg.isNotEmail)],
+};
