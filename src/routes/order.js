f2e24288ed6939081629956bac2d273a950ed7a9
Gabriel Rabelo Camargos <contato.ideialisa@gmail.com>
2020-08-22 10:56:53 -0300

    add error handler

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-
+import wrap from '../middleware/wrap';
 import OrderController from '../controllers/OrderController';
 
 const router = Router();
 
-router.post('/order', OrderController.store);
+router.post('/order', wrap(OrderController.store));
 
 module.exports = router;
