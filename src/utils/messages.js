188f1f31bcd03df039e7b0a5e4b35082e813c760
Gabriel Rabelo Camargos <contato.ideialisa@gmail.com>
2020-09-11 11:49:06 -0300

    add user validation

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,8 @@
 module.exports = {
   validation: {
     isNotEmail: 'Não é um email válido.',
+    isNotInvoiceType: 'Método de pagamento inválido.',
+    isNotPaymentTerms: 'Quantidade de parcelas inválidas',
+    mustBeExistis: 'O valor enviado, não foi encontrado',
   },
 };
--- src/validations/order.js
+++ src/validations/order.js
@@ -2,5 +2,15 @@ import { check } from 'express-validator';
 import { validation as msg } from '../utils/messages';
 
 module.exports = {
-  store: [check('email').isEmail().withMessage(msg.isNotEmail)],
+  store: [
+    check('addressId').exists().isInt(),
+    check('invoiceType')
+      .exists()
+      .matches(/card|banck-split/)
+      .withMessage(msg.isNotInvoiceType),
+    check('paymentTerms')
+      .isInt({ min: 1, max: 5 })
+      .if((value, { req }) => req.body.invoiceType === 'card')
+      .withMessage(msg.isNotPaymentTerms),
+  ],
 };
