101c128df01921504918e38069899479f6ce8588
Gabriel Rabelo <contato.ideialisa@gmail.com>
2020-10-10 23:36:31 -0300

    cleanup

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,9 @@
 import Sequelize from 'sequelize';
 import dbConfig from '../config/database';
 
-// import { Products, ProductsAttrs } from '../models';
 import Models from '../models';
 
 const connection = new Sequelize(dbConfig);
-
-// Products.init(connection);
-// ProductsAttrs.init(connection);
-
-// Products.associate(connection.models);
-// ProductsAttrs.associate(connection.models);
 
 Object.keys(Models).map((model) => Models[model].init(connection));
 Object.keys(Models).map((model) =>
