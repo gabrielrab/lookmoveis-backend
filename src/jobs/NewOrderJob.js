e26879600b5b6f3be50b5642a7da7390a7bb9d5c
Gabriel Rabelo <contato.ideialisa@gmail.com>
2020-12-29 22:45:12 -0300

    chore(new order client email): create template client email template by new order

--- src/jobs/NewOrderJob.js
+++ src/jobs/NewOrderJob.js
@@ -8,11 +8,24 @@ edge.registerViews(path.resolve(__dirname, '..', 'view'));
 export default {
   key: 'NewOrderJob',
   async handle({ data }) {
+    // company email
     await Mail.sendMail({
       from: process.env.MAIL_SENDER,
       to: process.env.MAIL_RECEIVER,
       subject: 'Nova solicitação de pedido',
       html: edge.render('newOrder', {
+        order: data,
+        products: data.products,
+        client: data.client,
+        address: data.address,
+      }),
+    });
+    // client email
+    await Mail.sendMail({
+      from: process.env.MAIL_SENDER,
+      to: data.client.email,
+      subject: 'Seu pedido foi adicionado com sucesso!',
+      html: edge.render('newOrderClient', {
         order: data,
         products: data.products,
         client: data.client,
