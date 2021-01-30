ebbbaaad9f23f2b9d3d1b99d0f6b38961999a62f
Gabriel Rabelo <contato.ideialisa@gmail.com>
2021-01-30 16:58:33 -0300

    chore(adminbro): connect with sequelize and import models from resources

--- src/config/adminBro.js
+++ src/config/adminBro.js
@@ -2,4 +2,5 @@ import 'dotenv/config';
 
 export default {
   url: process.env.ADMIN_BRO_URL,
+  companyName: process.env.ADMIN_BRO_COMPANY_NAME,
 };
--- src/services/adminBro.js
+++ src/services/adminBro.js
@@ -1,9 +1,29 @@
 import AdminBro from 'admin-bro';
 import AdminBroExpress from '@admin-bro/express';
+import AdminBroSequelize from '@admin-bro/sequelize';
+import adminBroConfigs from '../config/adminBro';
+import database from '../database';
+
+AdminBro.registerAdapter(AdminBroSequelize);
 
 const adminBro = new AdminBro({
-  database: [],
-  rootPath: '/admin',
+  database: [database],
+  rootPath: adminBroConfigs.url,
+  resources: [
+    { resource: database.models.Addresses },
+    { resource: database.models.Clients },
+    { resource: database.models.User },
+    { resource: database.models.WoodTypes },
+    { resource: database.models.ProductWoodTypes },
+    { resource: database.models.Categories },
+    { resource: database.models.Products },
+    { resource: database.models.Orders },
+  ],
+  branding: {
+    companyName: adminBroConfigs.companyName,
+    softwareBrothers: false,
+    logo: null,
+  },
 });
 const adminBroRouter = AdminBroExpress.buildRouter(adminBro);
 
