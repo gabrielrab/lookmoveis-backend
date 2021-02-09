620698630fbd35286cd6456f9f6a1d278f888659
Gabriel Rabelo <contato.ideialisa@gmail.com>
2021-02-09 09:49:59 -0300

    chore: get product data from backend

--- src/view/adminBro/components/show-image.products.tsx
+++ src/view/adminBro/components/show-image.products.tsx
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { BasePropertyProps } from 'admin-bro';
 import { Box } from '@admin-bro/design-system';
-// import ProductController from '../../../controllers/ProductController';
+import axios from 'axios';
+
+interface Images {
+  id: Number;
+  url: String;
+  type: String;
+}
 
 const ShowImage: React.FC<BasePropertyProps> = (props) => {
   const { record } = props;
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState<Images[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      console.log('[record]', record);
+      const { data } = await axios.get(`/products/${record.id}`, {
+        headers: {
+          apiKey: 12345678, // master key here,
+        },
+      });
+      setImages([...data.images, ...data.imagesDecorated]);
+    })();
+  }, [record]);
 
   const srcImg =
     'https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80';
@@ -16,7 +34,9 @@ const ShowImage: React.FC<BasePropertyProps> = (props) => {
         Maluco, se pá tu vai ter que me ajudar a criar uns componentes
         aqui kkkkkkk
       </p>
-      <img src={srcImg} width="200px" />
+      {images.map((image) => (
+        <img src={`/static/${image.url}`} width="200px" />
+      ))}
     </Box>
   );
 };
