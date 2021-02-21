b993d7b01f6249d67fbcbe304cca9f54316eee2a
Gabriel Rabelo <contato.ideialisa@gmail.com>
2021-02-21 19:59:45 -0300

    chore: show categorie image

--- src/lib/adminBroResources/CategoryResource.js
+++ src/lib/adminBroResources/CategoryResource.js
@@ -1,12 +1,31 @@
+import AdminBro from 'admin-bro';
 import adminBroConfigs from '../../config/adminBro';
 
 const properties = {
   id: {
+    isTitle: true,
     isVisible: {
       list: true,
       filter: false,
       show: true,
       edit: false,
+    },
+    type: 'string',
+  },
+  name: {
+    position: 1,
+  },
+  thumb: {
+    isVisible: {
+      list: false,
+      filter: false,
+      show: true,
+      edit: false,
+    },
+    components: {
+      show: AdminBro.bundle(
+        '../../view/adminBro/components/show-image.categories.tsx',
+      ),
     },
   },
   updatedAt: {
