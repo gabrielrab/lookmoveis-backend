17b5e0818f1ddc72e7557d59f30f1bcd0edaee2f
Gabriel Rabelo <contato.ideialisa@gmail.com>
2021-03-05 12:03:00 -0300

    chore: push cors

--- src/app.js
+++ src/app.js
@@ -18,6 +18,35 @@ const server = http.Server(app);
 setQueues(Queue.queues.map((queue) => new BullAdapter(queue.bull)));
 
 app.use(cors());
+app.use((req, res, next) => {
+  let oneof = false;
+  if (req.headers.origin) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    oneof = true;
+  }
+  if (req.headers['access-control-request-method']) {
+    res.header(
+      'Access-Control-Allow-Methods',
+      req.headers['access-control-request-method'],
+    );
+    oneof = true;
+  }
+  if (req.headers['access-control-request-headers']) {
+    res.header(
+      'Access-Control-Allow-Headers',
+      req.headers['access-control-request-headers'],
+    );
+    oneof = true;
+  }
+  if (oneof) {
+    res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+  }
+  if (oneof && req.method === 'OPTIONS') {
+    res.send(204);
+  } else {
+    next();
+  }
+});
 
 app.use(
   morgan(
