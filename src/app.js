289c843780b6f350d3d0166a713659a4be57c666
Gabriel Rabelo <contato.ideialisa@gmail.com>
2021-03-05 23:02:06 -0300

    chore: update generate policy

--- src/app.js
+++ src/app.js
@@ -19,8 +19,8 @@ setQueues(Queue.queues.map((queue) => new BullAdapter(queue.bull)));
 
 app.use(cors());
 app.use((req, res, next) => {
-  // res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
