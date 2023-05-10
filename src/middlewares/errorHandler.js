d1c3d87aa6edcc0c57d2b90766d854ea375c82cb
Gabriel Rabelo <contato.ideialisa@gmail.com>
2023-05-10 09:31:31 -0300

    chore: change template

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,6 +2,7 @@ const errorHandler = (err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
+  let gabriel;
   const error = err;
   return res
     .status(error.statusCode || 500)
--- src/routes/random.js
+++ src/routes/random.js
@@ -5,6 +5,7 @@ const router = Router();
 router.get('/random', (req, res) => {
   return res.send({
     random: Math.random(),
+    password: '123',
   });
 });
 
